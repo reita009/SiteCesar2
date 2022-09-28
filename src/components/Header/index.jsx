1009911604baa5b9d8658d18e3aeba0022ddb9dd
reita009 <julio.python777@gmail.com>
2022-09-28 20:30:12 -0300

    Terminando Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import Logo from "../../assets/logo.jpeg"
 import * as C from "./style";
+
 
 export const Header = () =>{
 
     return(
         <>
-            
+            <C.Container className="bg-gray-800 flex shadow-xl ">
+                <C.Img src={ Logo } className=""/>
+                <C.Btn>ASSINAR</C.Btn>
+            </C.Container>
         </>
     )
 }
