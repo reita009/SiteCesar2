d84679eb683881e17aaf66fa195fee9682dc4647
reita009 <julio.python777@gmail.com>
2022-09-28 21:12:08 -0300

    iniciando section one

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -3,6 +3,45 @@ import styled from "styled-components";
 
 export const Container = styled.header`
     width: 100vw;
-    height: 90px;
-    background-color: blue;
+    height: 130px;
+    padding: 10px;
+
+    @media (min-width: 760px) {
+        justify-content: space-between;
+    }
+    @media (max-width: 760px) {
+        flex-direction: column;
+        height: 180px;
+    }
+`;
+
+export const Img = styled.img`
+    height: 100px;
+    width: 100px;
+    border-radius: 30px;
+    margin: auto;
+
+
+    @media (min-width: 760px) {
+        margin: 0;
+    }
+`;
+export const Btn = styled.button`
+    background-color: #eb2f06;
+    padding: 2px;
+    height: 40px;
+    width: 100px;
+    border-radius: 10px;
+    align-self: center;
+    margin-right: 20px;
+    color: #ccc;
+
+    &:hover{
+        background-color:#b53428;
+    }
+    @media (max-width: 760px) {
+        width: 80%;
+        margin-left: 27px;
+    }
+   
 `;
