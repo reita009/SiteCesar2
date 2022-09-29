b93164e436bb909986c22a638b5a73b3f7560ac2
reita009 <julio.python777@gmail.com>
2022-09-28 22:08:25 -0300

    ajustando cores

---
+++ src/components/Section_1/style.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+    width: 100vw;   
+    flex-direction: column;
+    padding-bottom: 0%;
+    .wrap{
+        display: flex;
+        flex-direction: column;
+
+        .body{
+            margin-top: 20px;
+            justify-content: space-between;
+             display: flex;
+             flex-direction: column;
+
+            @media (min-width: 760px) {
+                flex-direction: row;
+            }
+        }
+
+        @media (min-width: 760px) {
+            width: 80%;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    }
+`;
+
+export const Img = styled.img`
+    height: 600px;
+    width: 400px;
+`;
+export const Title = styled.h1`
+    font-size: 60px;
+    text-align: center;
+    margin-top: 15px;
+`;
+export const Form = styled.form`
+    height: 600px;
+    width: 300px;
+    background-color: #1F2937;
+    border-radius: 20px;
+    margin-left: auto;
+    margin-right: auto;
+
+    @media (min-width: 760px) {
+                margin: 0;
+            }
+`;
